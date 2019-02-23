9b500e68e87da1aa9c88c4574952c94dd9639e5f
Viktor Golubov <golubovviktor@mail.ru>
2019-02-23 18:18:23 +0100

    React sample

--- src/main/resources/public/components/Article.js
+++ src/main/resources/public/components/Article.js
@@ -2,7 +2,7 @@ const Article = (props) => (
     <div>
         <h1>{props.title}</h1>
         <p>{props.text}</p>
-        <a><Link text="Google" url="http://www.google.com" /></a>
+        <Link text="Google" url="http://www.google.com" />
     </div>
 
 
